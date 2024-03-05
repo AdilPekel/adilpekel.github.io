8ac0178a4698ed6ce2ad0c4cd3879418b3c4674e
Adil Pekel <71811390+AdilPekel@users.noreply.github.com>
2024-03-04 17:25:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ window.addEventListener("scroll", function() {
     }
 })
 
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+};
+
 const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
